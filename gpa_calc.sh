3d04d0976e2ba4a7a16c680d11ec84d78cd6fcd5
Kyle1668 <kyledevinobrien1@gmail.com>
2017-10-31 18:44:09 -0700

    init

---
+++ gpa_calc.sh
@@ -0,0 +1,19 @@
+# GPA Calculator
+
+function printPrompt() {
+	echo 
+	echo
+	echo "College GPA Calculator"
+	echo "Enter the course name, number of units, and grade."
+	echo "Enter \"-1\" to stop"
+	echo
+	return
+}
+
+function getGrade() {
+
+}
+
+printPrompt "Kyle O'Brien"
+
+
